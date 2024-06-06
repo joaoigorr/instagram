049dfe2846716d871c9255cdf8a0979ce3c22dea
joaoigor <joao.igorr@gmail.com>
2024-06-06 17:52:23 -0300

    new version app

--- home.py
+++ home.py
@@ -23,48 +23,39 @@ hour_att_new = hour_att.strftime("%d/%m/%Y, %H:%M")
 st.text(f'Atualizado em: {hour_att_new}')
 
 #Métricas
-follow_min = int(df['followers'].min())
-follow_max = int(df['followers'].max())
-df_nt = df[df['username'] == 'cs2nicetry']
+#Nicetry
+df_nt = df[df['username'] == 'cs2nicetry'].sort_values(by='date')
+df_nt = df_nt.tail(2)
 follow_nt = int(df_nt['followers'].max())
-col1, col2, col3 = st.columns(3)
+follow_nt_last = int(df_nt['followers'].min())
+#Fallen da depre
+df_fd = df[df['username'] == 'fallendadepre'].sort_values(by='date')
+df_fd = df_fd.tail(2)
+follow_fd = int(df_fd['followers'].max())
+follow_fd_last = int(df_fd['followers'].min())
+delta_fd = (follow_fd - follow_nt ) - (follow_fd_last - follow_nt_last)
+#Draft5gg
+df_dt = df[df['username'] == 'draft5gg'].sort_values(by='date')
+df_dt = df_dt.tail(2)
+follow_dt = int(df_dt['followers'].max())
+follow_dt_last = int(df_dt['followers'].min())
+delta_dt = (follow_dt - follow_nt ) - (follow_dt_last - follow_nt_last)
+
+df_tc = df[df['username'] == 'draft5gg']
+follow_dt = int(df_tc['followers'].max())
+
+col1, col2, col3, col4 = st.columns(4)
 with col1:    
-    st.metric(label='Seguidores', value=follow_nt)
+    st.metric(label='Seguidores', value=follow_nt, delta= follow_nt - follow_nt_last)
 with col2:
-    st.metric(label='Distancia para 50k', value=follow_nt-50000)
+    st.metric(label='Distancia para Fallendadepre', value= follow_fd - follow_nt , delta = delta_fd, delta_color='inverse')
 with col3:
-    st.metric(label='Distancia para Fallendadepre', value=follow_nt - follow_max)
+    st.metric(label='Distancia para Dratf5', value= follow_dt - follow_nt, delta = delta_dt, delta_color='inverse')
+with col4:
+    st.metric(label='Distancia para 50k', value=50000-follow_nt)
 #Grafico de barras
 df_atual = df.sort_values(by='date')
 df_atual = df_atual.tail(6)
-
-# #Imagens
-# df_atual['link_image'] = df_atual['url_photo'].apply(lambda x: x + '.png')
-# df_editor = df_atual[['followers',
-#                       'username',
-#                       'link_image']].sort_values(by='followers',ascending=False)
-
-
-
-# st.data_editor(
-#     df_editor,
-#     column_config= {
-#         "followers": st.column_config.ProgressColumn(
-#             "Followers",
-#             format="%f",
-#             min_value = follow_min,
-#             max_value = follow_max
-#             )
-#     ,
-#      "link_image": st.column_config.ImageColumn(
-#             'Logo', help='test2'
-#     )
-# },
-#     hide_index=True,
-# )
-
-
-# #st.bar_chart(df_atual.sort_values('followers'), x='username', y='followers')
 
 fig_total = px.bar(df_atual.sort_values("followers"), x='followers',y='username', 
                        title='Total seguidoress',
@@ -78,4 +69,5 @@ st.plotly_chart(fig_total)
 st.subheader('Evolução no tempo.')
 df_line = df[['date','followers','username']]
 df_line = df_line.sort_values(by='date',ascending=False)
-st.line_chart(df_line, x='date', y='followers', color='username')
+fig_line = px.line(df_line, x='date', y='followers', color='username',markers=True)
+st.plotly_chart(fig_line)
